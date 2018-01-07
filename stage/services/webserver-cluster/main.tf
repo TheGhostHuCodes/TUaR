eb34afb30bf8da9c56602502445738f816600797
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2018-01-07 07:52:44 -0800

    Add s3 state storage for webserver-cluster terraform file

--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket  = "tuar-state"
+    key     = "stage/services/webserver-cluster/terraform.tfstate"
+    region  = "us-west-2"
+    encrypt = true
+  }
+}
+
 provider "aws" {
   region = "us-west-2"
 }
