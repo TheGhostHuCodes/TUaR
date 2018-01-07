fcab1289530ac458420df1f77054c402f77d13b4
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2018-01-07 09:11:16 -0800

    Add mysql data-store

---
+++ stage/data-stores/mysql/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  backend "s3" {
+    bucket  = "tuar-state"
+    key     = "stage/data-stores/mysql/terraform.tfstate"
+    region  = "us-west-2"
+    encrypt = true
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_db_instance" "example" {
+  engine            = "mysql"
+  allocated_storage = 10
+  instance_class    = "db.t2.micro"
+  name              = "example_database"
+  username          = "admin"
+  password          = "${var.db_password}"
+}
