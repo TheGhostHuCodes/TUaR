5a5cdaf6901063964f76c184d85cc892e5a0c0a0
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2018-01-07 11:16:15 -0800

    Refactor BASH script out of webserver-cluster terraform file

--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -23,18 +23,21 @@ data "terraform_remote_state" "db" {
   }
 }
 
+data "template_file" "user_data" {
+  template = "${file("user-data.sh")}"
+
+  vars {
+    server_port = "${var.server_port}"
+    db_address  = "${data.terraform_remote_state.db.address}"
+    db_port     = "${data.terraform_remote_state.db.port}"
+  }
+}
+
 resource "aws_launch_configuration" "example" {
   image_id        = "ami-0def3275"
   instance_type   = "t2.micro"
   security_groups = ["${aws_security_group.instance.id}"]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World!" > index.html
-              echo "${data.terraform_remote_state.db.address}" >> index.html
-              echo "${data.terraform_remote_state.db.port}" >> index.html
-              nohup busybox httpd -f -p "${var.server_port}" &
-              EOF
+  user_data       = "${data.template_file.user_data.rendered}"
 
   lifecycle {
     create_before_destroy = true
