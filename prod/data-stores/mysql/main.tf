61d8cde1e2a65ffed2f785ef380375e3b52fa048
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2018-01-07 20:37:43 -0800

    Add prod deployment of mysql data-store

---
+++ prod/data-stores/mysql/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  backend "s3" {
+    bucket  = "tuar-state"
+    key     = "prod/data-stores/mysql/terraform.tfstate"
+    region  = "us-west-2"
+    encrypt = true
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+module "mysql" {
+  source = "../../../modules/data-stores/mysql"
+
+  allocated_storage = 10
+  instance_class    = "db.t2.micro"
+  db_name           = "example_prod_database"
+  db_username       = "admin"
+  db_password       = "${var.db_password}"
+}
