3962fd93186016b4cb7964d9edc31df1bcd05163
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2018-01-09 13:21:54 -0800

    Add autoscaling schedule for autoscaling group in prod

--- prod/services/webserver-cluster/main.tf
+++ prod/services/webserver-cluster/main.tf
@@ -22,3 +22,23 @@ module "webserver_cluster" {
   min_size      = 3
   max_size      = 10
 }
+
+resource "aws_autoscaling_schedule" "scale_out_during_business_hours" {
+  scheduled_action_name = "scale_out_during_business_hours"
+  min_size              = 3
+  max_size              = 4
+  desired_capacity      = 4
+  recurrence            = "0 9 * * *"
+
+  autoscaling_group_name = "${module.webserver_cluster.asg_name}"
+}
+
+resource "aws_autoscaling_schedule" "scale_in_at_night" {
+  scheduled_action_name = "scale_in_at_night"
+  min_size              = 3
+  max_size              = 4
+  desired_capacity      = 3
+  recurrence            = "0 17 * * *"
+
+  autoscaling_group_name = "${module.webserver_cluster.asg_name}"
+}
