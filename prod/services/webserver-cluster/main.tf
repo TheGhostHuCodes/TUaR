39c93db2ee54dac94efd1975f2556cd56f8890bc
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2018-01-09 17:59:45 -0800

    Refactor autoscaling_schedule to conditionally run in prod

--- prod/services/webserver-cluster/main.tf
+++ prod/services/webserver-cluster/main.tf
@@ -18,27 +18,8 @@ module "webserver_cluster" {
   db_remote_state_bucket = "tuar-state"
   db_remote_state_key    = "prod/data-stores/mysql/terraform.tfstate"
 
-  instance_type = "t2.micro"
-  min_size      = 3
-  max_size      = 10
-}
-
-resource "aws_autoscaling_schedule" "scale_out_during_business_hours" {
-  scheduled_action_name = "scale_out_during_business_hours"
-  min_size              = 3
-  max_size              = 4
-  desired_capacity      = 4
-  recurrence            = "0 9 * * *"
-
-  autoscaling_group_name = "${module.webserver_cluster.asg_name}"
-}
-
-resource "aws_autoscaling_schedule" "scale_in_at_night" {
-  scheduled_action_name = "scale_in_at_night"
-  min_size              = 3
-  max_size              = 4
-  desired_capacity      = 3
-  recurrence            = "0 17 * * *"
-
-  autoscaling_group_name = "${module.webserver_cluster.asg_name}"
+  instance_type      = "t2.micro"
+  min_size           = 3
+  max_size           = 10
+  enable_autoscaling = true
 }
